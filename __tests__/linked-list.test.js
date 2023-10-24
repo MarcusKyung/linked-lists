8ea7ded26322e8c298aa4bba5d460f80f702bd88
MarcusKyung <Kyungmj@gmail.com>
2023-10-23 18:57:58 -0700

    add: initial file setup/tests

---
+++ __tests__/linked-list.test.js
@@ -0,0 +1,94 @@
+import LinkedList from '../src/linked-list.js';
+
+describe('LinkedList', () => {
+
+  let linkedListWithNodes = new LinkedList(); //creates new linkedlist with nodes then adds some sample nodes
+
+  beforeEach(() => {
+    linkedListWithNodes.insertLast("node1");
+    linkedListWithNodes.insertLast("node2");
+    linkedListWithNodes.insertLast("node3");
+    linkedListWithNodes.insertLast("node4");
+  });
+
+  afterEach(() => {
+    linkedListWithNodes.head = null; //removing the head of a linked list is the equivalant of deleting it. Use it here to keep tests from impacting each other.
+  });
+
+  test('it should construct a linked list with a head property', () => {
+    let linkedList = new LinkedList();
+    expect(linkedList.head).toEqual(null) //will be null ebcause it is an empty linked list for now
+  })
+
+  test('LinkedList.prototype.insertLast() should add a node at head if a linked list has no head', () => {
+    let linkedList = new LinkedList();
+    linkedList.insertLast("head");
+    expect(linkedList.head.data).toEqual("head");
+  })
+
+  test('LinkedList.prototype.insertLast() should add a node at the end of a linked list', () => {
+    linkedListWithNodes.insertLast("new last");
+    expect(linkedListWithNodes.head.next.next.next.next.data).toEqual("new last");
+  })
+
+// TESTS FOR REMOVE() //
+  test('it should set a new head if the head is removed', () => {
+    linkedListWithNodes.remove(0);
+    expect(linkedListWithNodes.head.data).toEqual("node2");
+  })
+
+  //In this test, we are removing the third node — and then checking to see if the new third node is equal to the previous fourth node. 
+  //That will verify the old third node has been removed.
+  test('it should remove a node from the middle', () => { 
+    linkedListWithNodes.remove(2);
+    expect(linkedListWithNodes.head.next.next.data).toEqual("node4")
+  })
+
+  test('it should return -1 if the index does not exist', () => {
+    expect(linkedListWithNodes.remove(9)).toEqual(-1);
+  });
+
+// TESTS FOR COUNT() //
+  test('it should count the total number of nodes in the linked list', () => {
+    expect(linkedListWithNodes.count()).toEqual(4)
+  })
+
+  test('it should count the total number of nodes in the linked list', () => {
+    linkedListWithNodes.insertLast("new last");
+    expect(linkedListWithNodes.count()).toEqual(5)
+  })
+
+  test('it should count the total number of nodes in the linked list', () => {
+    let linkedList = new LinkedList();
+    expect(linkedList.count()).toEqual(0)
+  })
+
+  test('it should count the total number of nodes in the linked list', () => {
+    let linkedList = new LinkedList();
+    linkedList.insertLast("addNode");
+    expect(linkedList.count()).toEqual(1)
+  })
+
+// TESTS FOR ADDFIRST() //
+  test('it should add a new node at teh beginning of the linked list', () => {
+    linkedListWithNodes.addFirst("NewHead");
+    expect(linkedListWithNodes.head.data).toEqual("NewHead")
+  })
+
+// TESTS FOR INSERTATINDEX() //
+  test('it should insert a node at a specified index', () => { //This test puts the node into a specified position
+    linkedListWithNodes.insertAtIndex(2, "inserted info");
+    expect(linkedListWithNodes.head.next.next.data).toEqual("inserted info");
+  })
+
+  test('it should insert a node at end if specified index is greater than linked list nodes', () => { //this test puts node at end if index is longer than list length
+    linkedListWithNodes.insertAtIndex(100, "inserted data");
+    expect(linkedListWithNodes.head.next.next.next.next.data).toEqual("inserted data");
+  })
+
+  test('it should insert a node at the start if there are no nodes in the linked list', () => {
+    let linkedList = new LinkedList();
+    linkedList.insertAtIndex(3, "inserted node");
+    expect(linkedList.head.data).toEqual("inserted node");
+  })
+})
---
+++ src/linked-list.js
@@ -0,0 +1,86 @@
+// since the head node is a reference for all other nodes it is good to avoid changing it. Sometimes devs will declare it a symbol (immutable primative data type which doesn't change).
+import Node from './node.js'
+
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertLast(data) {
+    const newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head; //starts at head
+      while (currentNode.next != null) { //loops through nodes while this.next is not null. This is traversing the chain.
+        currentNode = currentNode.next
+      }
+      currentNode.next = newNode; //sets currentNode.next property to newly created node in order to add it to list. 
+    }
+  }
+
+  remove(indexToRemove) {
+    if (indexToRemove === 0) { //removing the head index
+      this.head = this.head.next;
+    } else {
+      let currentNode = this.head;
+      let currentIndex = 0;
+      while ((currentIndex +1) <= indexToRemove) { 
+        if (currentNode.next === null ) {
+          return -1;
+        }
+        if ((currentIndex + 1 ) === indexToRemove) { //This tells us we have arrived at the node directly before the node we want to remove. Instead of going to the one we want to remove we go to the one right before.
+          currentNode.next = currentNode.next.next; //This sets the current node's next to the one after the one we are removing, thus removing the one we were targeting
+          break;
+        }
+        currentNode = currentNode.next; //if the condition isn't found these lines advance the current node and increment the counter
+        currentIndex++;
+      }
+    }
+  }
+
+  count() {
+    if (this.head === null) {
+      return 0;
+    } else if (this.head.next === null) {
+      return 1;
+    } else {
+      let currentNode = this.head;
+      let nodeCounter = 1;
+      while (currentNode.next != null) {
+        currentNode = currentNode.next;
+        nodeCounter++;
+        // console.log(nodeCounter);
+      }
+      return nodeCounter;
+    }
+  }
+
+
+  addFirst(data) {
+    let newNode = new Node(data);
+    let currentHead = this.head;
+    newNode.next = currentHead;
+    this.head = newNode
+  }
+
+  insertAtIndex(index, data) {
+    let newNode = new Node(data)
+    
+    if (this.count() === 0) {
+      this.head = newNode;
+    } else if (index > this.count()) {
+      this.insertLast(data)
+    } else {
+      let currentNode = this.head;
+      let currentIndex = 0;
+      while (currentNode && currentIndex < index - 1) {
+          currentNode = currentNode.next;
+          currentIndex++;
+      }
+      newNode.next = currentNode.next;
+      currentNode.next = newNode;
+    }
+  }
+}
+
